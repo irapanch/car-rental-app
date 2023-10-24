1112e883cc9d51d680d4bd933de2703ae5819dcf
irapanch <117355580+irapanch@users.noreply.github.com>
2023-10-24 09:55:43 +0300

    add global styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/car-rental-app">
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 );
---
+++ src/styles/theme.jsx
@@ -0,0 +1,54 @@
+export const theme = {
+  colors: {
+    black: '#140e02',
+    white: '#ebe1ce',
+    text: '#081338',
+    background: '#7cb9ff',
+    cardBackground: '#1a63c4',
+    lightAccent: '#26deff',
+    accent: '#206ea1',
+    muted: '#a9d8db',
+    btnPrimary: '#1f97a7',
+    btnSecondary: '#82b1b1',
+    error: '#db680a',
+    gradient: 'linear-gradient(141.22deg, #386b58 9.4%, #4081b6 91.91%)',
+  },
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'Delicious Handrawn, cursive',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: {
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    ml: '18px',
+    l: '20px',
+    xl: '28px',
+    xxl: '64px',
+  },
+  fontWeights: {
+    normal: 400,
+    medium: 500,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    normal: '4px',
+    round: '50%',
+  },
+  shadows: {
+    textShadow: '0 1px 1px #fff',
+    boxShadowHeader: '8px 8px 8px -8px #000000, -4px 0px 8px 0px #000000',
+    boxShadowDiv: '10px 10px 18px -12px #000000',
+    boxShadowSearch: '8px 8px 8px -8px #000000, -1px 5px 8px 0px #000000',
+  },
+};
