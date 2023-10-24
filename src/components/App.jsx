70956d1c5fc1c685a7446b79a8df61b131700a33
irapanch <117355580+irapanch@users.noreply.github.com>
2023-10-24 14:36:57 +0300

    add suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout/Layout';
-import HomePage from 'pages/Home/HomePage';
-import CatalogPage from 'pages/CatalogPage/CatalogPage';
-import UserFavoritePage from 'pages/UserFavoritePage/UserFavoritePage';
+import HomePage from '../pages/Home/HomePage';
+import CatalogPage from '../pages/CatalogPage/CatalogPage';
+import UserFavoritePage from '../pages/UserFavoritePage/UserFavoritePage';
 
 export const App = () => {
   return (
@@ -10,8 +10,8 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/favorites" element={<UserFavoritePage />} />
+          <Route path="catalog" element={<CatalogPage />} />
+          <Route path="favorites" element={<UserFavoritePage />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -5,21 +5,27 @@ import {
   StyledLogo,
   StyledNavLink,
 } from './Layout.styled';
-import logoImg from '../img/alert-img.png';
+import logoImg from '../../img/alert-img.png';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Loader } from 'components/Loader/Loader';
 
 const Layout = () => {
   return (
     <StyledContainer>
       <StyledHeader>
+        <StyledLogo to="/">
+          <img src={logoImg} alt="logoImg" />
+        </StyledLogo>
         <nav>
           <StyledNavLink to="/">Home</StyledNavLink>
           <StyledNavLink to="/catalog">Catalog</StyledNavLink>
           <StyledNavLink to="/favorites">Favorites</StyledNavLink>
         </nav>
-        <StyledLogo to="/">
-          <img src={logoImg} alt="logoImg" />
-        </StyledLogo>
       </StyledHeader>
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
     </StyledContainer>
   );
 };
